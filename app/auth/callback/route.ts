39efc5e71b478efd923fd7bc70d3920eaa58fda6
lachitooo <39280428+lazarobeas@users.noreply.github.com>
2025-03-04 20:46:52 -0600

    Fixed landing page apostrophe syntax error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,4 +1,3 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import {NextRequest, NextResponse} from "next/server";
 import {createClient} from "@/utils/supabase/server";
 import {createServiceRoleClient} from "@/utils/supabase/service-role";
