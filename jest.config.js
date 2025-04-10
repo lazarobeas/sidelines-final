638d5bb629ba41229f66c90f0919bb3c8b6c5e3c
Matthew Chapa <mdchapa47@gmail.com>
2025-04-10 13:57:05 -0500

    Removed using turbopack and made adjustment to compensate for it

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/$1'
-    },
-    testEnvironment: 'jsdom',
-    moduleDirectories: ['node_modules', '<rootDir>'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
-  };
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+const React = require('react');
 global.React = React;
 
 // Mock scrollIntoView
