a0c8d1386d6c98631f43c2f60bf83a4c61796b79
larintb <vlpizana@gmail.com>
2025-05-12 20:05:52 -0500

    Translation fixes

--- app/channels/[slug]/loading.tsx
+++ app/channels/[slug]/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
       <div className="h-screen w-screen bg-[#232341] flex flex-col items-center justify-center text-white">
-        <div className="animate-pulse text-2xl">Loading channel...</div>
+        <div className="animate-pulse text-2xl">Loading Channel...</div>
       </div>
     );
   }
